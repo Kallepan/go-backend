05ebd940cf6e12a6f75029370059848f516f0899
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-09-05 21:35:52 +0000

    feat: postgres database connection stump

--- src/app/domain/dao/base_model.go
+++ src/app/domain/dao/base_model.go
@@ -1,7 +1,13 @@
 package dao
 
-import "time"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type BaseModel struct {
-	CreatedAt time.Time `json:"created_at"`
+	CreatedAt time.Time      `gorm:"->:false;column:created_at" json:"-"`
+	UpdatedAt time.Time      `gorm:"->:false;column:updated_at" json:"-"`
+	DeletedAt gorm.DeletedAt `gorm:"->:false;column:deleted_at" json:"-"`
 }
---
+++ src/config/drivers/psql.go
@@ -0,0 +1,20 @@
+package drivers
+
+import (
+	"os"
+
+	log "github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectToDB() *gorm.DB {
+	dsn := os.Getenv("DB_DSN")
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect database. Error: ", err)
+	}
+
+	return db
+}
