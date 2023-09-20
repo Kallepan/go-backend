49854c4d3b461f2a409258ea815c2190c124108b
kajeepan kandeepan <kajeepan.kandeepan@gmail.com>
2023-09-20 18:39:15 +0000

    feat: added dco domain

--- src/app/repository/system_repository.go
+++ src/app/repository/system_repository.go
@@ -1,14 +1,20 @@
 package repository
 
+import "github.com/kallepan/go-backend/app/domain/dco"
+
 type SystemRepository interface {
-	GetVersion() (string, error)
+	GetVersion() (*dco.Version, error)
 }
 
 type SystemRepositoryImpl struct {
 }
 
-func (s SystemRepositoryImpl) GetVersion() (string, error) {
-	return "1.0.0", nil
+func (s SystemRepositoryImpl) GetVersion() (*dco.Version, error) {
+	return &dco.Version{
+		Major: 1,
+		Minor: 0,
+		Patch: 0,
+	}, nil
 }
 
 func SystemRepositoryInit() *SystemRepositoryImpl {
